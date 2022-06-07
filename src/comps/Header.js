a6b2f38df64fbdbc5c3b47b719272c2c20bb010d
Butters <k.butterfield08@gmail.com>
2022-06-07 12:33:55 -0500

    Getting the outline done

---
+++ src/comps/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+const Header = () =>{
+    return(
+        <div>
+            <header className="HeaderContainer">
+                <h1 className="HeaderSearch">Farm Tracker</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
