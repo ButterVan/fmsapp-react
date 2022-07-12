e850a8fc872f3193a2934b21f63343e688a1881c
Butters <k.butterfield08@gmail.com>
2022-07-12 18:58:11 -0500

    Getting the outline of list done
    Getting this list outline done., onto the next steps of feeding it data

---
+++ client/src/localData/localListData.js
@@ -0,0 +1,26 @@
+const herdList = [
+  {
+    "steelId": "123",
+    "tagId": "Y12",
+    "breed": "Sential",
+    "sex": "Cow"
+  },
+  {
+    "steelId": "124",
+    "tagId": "Y55",
+    "breed": "Sential",
+    "sex": "Cow"
+  },
+  {
+    "steelId": "224",
+    "tagId": "R43",
+    "breed": "Sential",
+    "sex": "Cow"
+  },
+  {
+    "steelId": "236",
+    "tagId": "W33",
+    "breed": "Sential",
+    "sex": "Cow"
+  }, 
+];
