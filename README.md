8cf20e903403f3c7a339ac5ddfe52d80a9052049
Butters <k.butterfield08@gmail.com>
2022-12-13 21:21:17 -0600

    Getting MERN closer to setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# FMSAPP - React Version
+
+FMSAPP - Farm Management System - will be a working web app for small farmers to use to manage their farm. 
+
--- backend/controllers/animalController.js
+++ backend/controllers/animalController.js
@@ -1,36 +1,42 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get animal
 // @route GET /api/animal
 // @access Private
-const getAnimal = (req, res) => { 
-  console.log(req.body)
-  res.status(200).json({message: "get animal"})
-}
+const getAnimal = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "get animal" });
+})
 
 // @desc create animal
 // @route POST /api/animal
 // @access Private
-const createAnimal = (req, res) => {
-  if (!req.body.text) { 
-    res.status(400).json({ message: "Please send with text" })
+const createAnimal = asyncHandler( async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please send with text");
   }
-  console.log(req.body.text)
-  res.status(200).json({message: "Create animal"})
-}
+  console.log(req.body.text);
+  res.status(200).json({ message: "Create animal" });
+})
 
 // @desc update animal
 // @route PUT /api/animal/:id
 // @access Private
-const updateAnimal = (req, res) => { 
-  res.status(200).json({message: `update animal ${req.params.id}`})
-}
+const updateAnimal = asyncHandler( async (req, res) => {
+  res.status(200).json({ message: `update animal ${req.params.id}` });
+})
 
 // @desc delete animal
 // @route DELETE /api/animal/:id
 // @access Private
-const deleteAnimal = (req, res) => { 
-  res.status(200).json({message: `delete animal ${req.params.id}`})
-}
+const deleteAnimal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `delete animal ${req.params.id}` });
+})
 
 module.exports = {
-  getAnimal, createAnimal, updateAnimal, deleteAnimal
-}
+  getAnimal,
+  createAnimal,
+  updateAnimal,
+  deleteAnimal,
+};
